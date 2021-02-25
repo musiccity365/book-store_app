8e342bca19401b029db74af88a8235a52716dd70
musiccity365 <m_sackstein@yahoo.com>
2021-02-25 15:24:13 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books
-  resources :users
-  resources :ratings, only: [:index, :show, :new, :create, :edit, :update] 
+  resources :users, only: [index, show, new, create] # nested routes - index, new ?
+  resources :ratings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
