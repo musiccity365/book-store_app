b9291493be2847a77e66fbe7ee3cf1657bb6c326
musiccity365 <m_sackstein@yahoo.com>
2021-02-25 00:18:43 -0500

    writing out controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "User successfully created"
+      redirect_to @user
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
 
 
 
