525978079155a53e9044d0467379860bc1939e63
musiccity365 <m_sackstein@yahoo.com>
2021-02-26 21:05:45 -0500

    gems upgrade
    rubygems upgrade from 2.6.1 ~> 3.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get '/books/oldest', to: 'books#oldest', as: 'oldest_book'
-  # get '/books', to: 'books#index', as: 'books'
-  # get '/books/:id', to: 'books#show', as: 'book'
   resources :books, only: [:index, :show]
   # resources :users, only: [index, show, new, create] # nested routes - index, new ?
   # resources :ratings
