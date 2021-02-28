2036a8fab59c7665e8f7e7651cdbb6076375380b
musiccity365 <m_sackstein@yahoo.com>
2021-02-28 00:14:22 -0500

    working on validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  validates :score, numericality: {greater_than_or_equal_to: 1}, presence: true
+  validates :score, numericality: {less_than_or_equal_to: 10}, presence: true
 end
