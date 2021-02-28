0df7a4635a0dcbf77e4e738d8112a3a037559a7b
musiccity365 <m_sackstein@yahoo.com>
2021-02-28 09:12:22 -0500

    usernamme and password validations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def home
   end
+
+  def new
+    @session = Session.new
+  end
+
+  def create
+    @session = Session.new(params[:session])
+    if @session.save
+      flash[:success] = "Session successfully created"
+      redirect_to login_path(@session)
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :ratings
   has_many :books, through: :ratings
-  # use uniqueness for login credential validations
+  has_secure_password # macro
+  validates :username, presence: :true # - active record validations - validates that a username is present using AR
+  validates :username, uniqueness: :true # - active record validations - validates that a username is unique and does not already exist using AR
 end
