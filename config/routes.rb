dda475d4855de86bdf186be916a7fb68e8b015c8
musiccity365 <m_sackstein@yahoo.com>
2021-02-27 20:42:59 -0500

    --- Auto-Git Commit ---

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:index, :show, :new, :create, :edit, :update]
-  resources :users, only: [index, show, new, create] # nested routes - index, new ?
+  resources :users, only: [:index, :show, :new, :create] # nested routes - index, new ?
   resources :ratings
 end
