9093ebc3e5e1c3175c2014ed79b47b5f3ca9de1e
musiccity365 <m_sackstein@yahoo.com>
2021-02-27 21:44:14 -0500

    --- Auto-Git Commit ---

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,13 @@
 class Book < ApplicationRecord
   has_many :ratings
   has_many :users, through: :ratings
-  validates :title, :author, :publisher, :genre, :score, :release_year, presence: true
+  validates :title, :author, :publisher, presence: true
   # validates
 
-  # def book_list # helper_method :book_list
-  #   "#{self.title} - #{self.release_year}"
-  # end
-  
-  # def display_release_date
-    
-  # end
+  def book_list # helper_method :book_list
+    "#{self.title} - #{self.release_year}"
+  end
+
+  def display_release_date
+  end
 end
