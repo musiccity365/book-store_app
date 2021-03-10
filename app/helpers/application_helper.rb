3399da7a64db096d71ec6e63f1295b310dee25f0
musiccity365 <m_sackstein@yahoo.com>
2021-03-09 22:00:31 -0500

    revising, and making changes that I probably shouldn't have, but I am an idiot ya know... lol

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,14 @@ module ApplicationHelper
   # helper_method :current_user, :logged_in?
   # before_action :require_login
   
-  # private
+  private
   
-  #   def require_login
-  #       unless logged_in?
-  #         flash[:error] = "You must be logged in to access this section"
-  #         redirect_to login_path
-  #       end
-  #   end
+    def require_login
+        unless logged_in?
+          flash[:error] = "You must be logged in to access this section"
+          redirect_to login_path
+        end
+    end
   # def nav_bar
   #   menu = ''
   #   if logged_in?
